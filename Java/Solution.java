5cbae70217d6d35c22be0b95d99fd6f7bf5fdae8
mdw22 <mdw22@email.sc.edu>
2024-05-31 17:20:46 -0700

    scoreofString solution

--- Java/Solution.java
+++ Java/Solution.java
@@ -577,4 +577,12 @@ public class Solution {
         if(root1_list.equals(root2_list)) return true;
         return false;
     }
+
+    public int scoreOfString(String s) {
+        int result = 0;
+        for(int i = 0; i < s.length() - 1; ++i) {
+            result += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return result;
+    }
 }
