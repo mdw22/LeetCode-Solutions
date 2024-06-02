dc73ebfa55443108a9109e52e22839b1ce3c8782
mdw22 <mdw22@email.sc.edu>
2024-06-02 15:33:33 -0700

    reverseString solution + others

--- Java/Solution.java
+++ Java/Solution.java
@@ -585,4 +585,12 @@ public class Solution {
         }
         return result;
     }
+
+    public void reverseString(char[] s) {
+        for(int i = 0, j = s.length - 1; i <= j; ++i, --j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+    }
 }
