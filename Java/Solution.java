aaab89e5f464b918bcb2ec0afff1fb475ec2b022
mdw22 <mdw22@email.sc.edu>
2024-06-09 17:42:32 -0700

    heightChecker solution

--- Java/Solution.java
+++ Java/Solution.java
@@ -808,4 +808,14 @@ public class Solution {
 
         return result;
     }
+
+    public int heightChecker(int[] heights) {
+        int[] expected = heights.clone();
+        Arrays.sort(expected);
+        int result = 0;
+        for (int i = 0; i < heights.length; ++i) {
+            if(heights[i] != expected[i]) result++;
+        }
+        return result;
+    }
 }
